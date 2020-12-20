3a86b59929dd17a4cfebc4c5591136f078a712a7
Iqbal AISSAOUI <aissaoui.iqbal@gmail.com>
2020-12-20 03:47:51 +0100

    restructure project

---
+++ demo/src/main/java/com/example/demo/api/BeersAPI.java
@@ -0,0 +1,65 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Beer;
+import com.example.demo.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BeersAPI {
+
+    private final String URL = "https://api.punkapi.com/v2/beers";
+    private final String URL_TEST = "https://httpbin.org/ip";
+
+    @Autowired
+    private BeerService beerService;
+
+
+    @GetMapping("/test")
+    public String test(){
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders hh = new HttpHeaders();
+        hh.setContentType(MediaType.APPLICATION_JSON);
+        hh.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        hh.add(HttpHeaders.USER_AGENT, "demo/1.0");
+
+        ResponseEntity<String> result = rt.exchange(URL, HttpMethod.GET,new HttpEntity<Object>(hh),String.class);
+
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+    @GetMapping("/allold")
+    public Beer[] getAllOld(){
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders hh = new HttpHeaders();
+        hh.setContentType(MediaType.APPLICATION_JSON);
+        hh.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        hh.add(HttpHeaders.USER_AGENT, "demo/1.0");
+
+
+        ResponseEntity<Beer[]> result = rt.exchange(URL, HttpMethod.GET,new HttpEntity<Object>(hh), Beer[].class);
+
+        new ArrayList<>(Arrays.asList(result.getBody())).forEach(System.out::println);
+
+        return result.getBody();
+    }
+    @GetMapping("/all")
+    public List<Beer> getAll() throws Exception {
+
+        return beerService.getAllBeers();
+    }
+
+
+}
---
+++ demo/src/main/java/com/example/demo/utils/LocalDateDeserializer.java
@@ -0,0 +1,47 @@
+package com.example.demo.utils;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+    private static final long serialVersionUID = 1L;
+    private static final String format1= "MM/yyyy";
+    private static final String format2= "yyyy";
+
+    protected LocalDateDeserializer() {
+        super(LocalDate.class);
+    }
+
+
+    @Override
+    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+
+        String toParse = jp.readValueAs(String.class);
+        LocalDate ld;
+
+        try{
+            YearMonth ym = YearMonth.parse(toParse, DateTimeFormatter.ofPattern(format1));
+            ld = ym.atDay(1);
+        }catch(DateTimeParseException dpe){
+            Year y = Year.parse(toParse, DateTimeFormatter.ofPattern(format2) );
+            ld = y.atDay(1);
+        }finally {
+
+        }
+
+        return ld;
+    }
+
+}
