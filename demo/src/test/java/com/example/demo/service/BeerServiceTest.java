b65b54ea19ee5b02df45ff4259ad022ef0bb7bf5
Iqbal AISSAOUI <aissaoui.iqbal@gmail.com>
2020-12-21 23:17:45 +0100

    first test with Mockito/JUnit 5

---
+++ demo/src/test/java/com/example/demo/service/BeerServiceTest.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Beer;
+import com.example.demo.repo.BeerRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@ExtendWith(MockitoExtension.class)
+class BeerServiceTest {
+
+    @InjectMocks
+    BeerService beerService;
+
+    @Mock
+    BeerRepository beerRepository;
+
+    @BeforeAll
+    public static void init() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        Mockito.lenient().when(beerRepository.save(Mockito.any(Beer.class))).then(AdditionalAnswers.returnsFirstArg());
+        Mockito.lenient().when(beerRepository.saveAll(Mockito.anyList())).then(AdditionalAnswers.returnsFirstArg());
+    }
+
+    @Test
+    void saveBeer() {
+        Beer beerArg = new Beer();
+        beerArg.setId(1L);
+        Beer beerReturn = beerService.saveBeer(beerArg);
+        Assertions.assertTrue(beerArg.equals(beerReturn));
+    }
+
+    @Test
+    void saveBeers() {
+        Beer beerArg = new Beer();
+        beerArg.setId(1L);
+        List<Beer> listBeers = Arrays.asList(beerArg);
+        List<Beer> beerReturn = beerService.saveBeers(listBeers);
+        Assertions.assertTrue(listBeers.equals(beerReturn));
+    }
+
+    @Test
+    void getBeer() {
+    }
+
+    @Test
+    void getAllBeers() {
+    }
+}
